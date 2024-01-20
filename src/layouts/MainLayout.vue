5cdc55d75e00fd9864eaa45be095f8440635628b
Charlesson-MP <charlessonmendes@gmail.com>
2024-01-20 16:05:05 -0300

    Configurando o q-header do main layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated style="height: 60px; background-color: rgb(11, 11, 83);">
       <q-toolbar>
         <q-btn
           flat
@@ -12,7 +12,13 @@
         />
 
         <q-toolbar-title>
-          Quasar App
+          <q-avatar>
+            <img
+              alt="logo-manager-edu-pequeno"
+              style="width: 60px;"
+              src="../assets/Brasão_Vitoria_da_Conquista.svg">
+          </q-avatar>
+          Estagi<span style="color: yellow;">Hub</span>
         </q-toolbar-title>
 
         <div>Quasar v{{ $q.version }}</div>
@@ -20,8 +26,9 @@
     </q-header>
 
     <q-drawer
+      :breakpoint="700"
       v-model="leftDrawerOpen"
-      show-if-above
+      :show-if-above="false"
       bordered
     >
       <q-list>
