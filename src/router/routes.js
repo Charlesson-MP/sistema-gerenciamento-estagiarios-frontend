4359aef9daa7383797c4dd4a162d255ea6b88699
Charlesson-MP <charlessonmendes@gmail.com>
2024-01-20 01:00:58 -0300

    Criando o layout e login page e configurando suas rotas

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,6 +1,13 @@
 const routes = [
   {
     path: '/',
+    component: () => import('layouts/LoginLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/LoginPage.vue') }
+    ]
+  },
+  {
+    path: '/main',
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') }
