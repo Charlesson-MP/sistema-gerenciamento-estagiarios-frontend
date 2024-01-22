815c7e22a6e47f4b07efeface747e241743f5f04
Charlesson-MP <charlessonmendes@gmail.com>
2024-01-22 14:21:33 -0300

    Corrigindo indentação no card usuário

--- src/components/CardUsuario.vue
+++ src/components/CardUsuario.vue
@@ -38,58 +38,58 @@ const rows = [
 <template>
   <q-card class="my-card">
     <q-toolbar-title id="cabecalho">
-          <q-avatar>
-            <img
-              alt="logo-prefeitura"
-              src="../assets/group_FILL0_wght400_GRAD0_opsz24.svg"
-              style="width: 30px;"
-            >
-          </q-avatar>
-          Usuários
-        </q-toolbar-title>
-      <q-card-section v-if="!novoUsuario">
-        <div class="row">
-          <q-btn class="col-2 q-mr-xl" color="primary" @click="addNovoUsuario">+ Novo Usuário</q-btn>
-          <q-input class="col-6 q-ml-xl" filled label="Pesquisar ...">
-            <template v-slot:prepend>
-              <q-icon name="search" />
-            </template>
-          </q-input>
+      <q-avatar>
+        <img
+          alt="logo-prefeitura"
+          src="../assets/group_FILL0_wght400_GRAD0_opsz24.svg"
+          style="width: 30px;"
+        >
+      </q-avatar>
+      Usuários
+    </q-toolbar-title>
+    <q-card-section v-if="!novoUsuario">
+      <div class="row">
+        <q-btn class="col-2 q-mr-xl" color="primary" @click="addNovoUsuario">+ Novo Usuário</q-btn>
+        <q-input class="col-6 q-ml-xl" filled label="Pesquisar ...">
+          <template v-slot:prepend>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+      </div>
+    </q-card-section>
+    <q-card-section v-if="!novoUsuario">
+      <div class="q-pa-md">
+        <q-table
+          separator="cell"
+          flat bordered
+          title="Lista de usuários"
+          :rows="rows"
+          dense
+          :columns="columns"
+          row-key="name"
+        />
+      </div>
+    </q-card-section>
+    <q-card-section v-if="novoUsuario">
+      <div class="row">
+        <div class="col-6">
+          <q-input class="q-ma-md" filled label="Nome"></q-input>
+          <q-input class="q-ma-md" filled label="E-mail"></q-input>
+          <q-input class="q-ma-md" filled label="Telefone"></q-input>
+          <q-input class="q-ma-md" filled label="Perfil"></q-input>
         </div>
-      </q-card-section>
-      <q-card-section v-if="!novoUsuario">
-        <div class="q-pa-md">
-          <q-table
-            separator="cell"
-            flat bordered
-            title="Lista de usuários"
-            :rows="rows"
-            dense
-            :columns="columns"
-            row-key="name"
-          />
+        <div class="col-6">
+          <q-input class="q-ma-md" filled label="Status"></q-input>
+          <q-input class="q-ma-md" filled label="Senha"></q-input>
+          <q-input class="q-ma-md" filled label="Repetir senha"></q-input>
         </div>
-      </q-card-section>
-      <q-card-section v-if="novoUsuario">
-        <div class="row">
-          <div class="col-6">
-            <q-input class="q-ma-md" filled label="Nome"></q-input>
-            <q-input class="q-ma-md" filled label="E-mail"></q-input>
-            <q-input class="q-ma-md" filled label="Telefone"></q-input>
-            <q-input class="q-ma-md" filled label="Perfil"></q-input>
-          </div>
-          <div class="col-6">
-            <q-input class="q-ma-md" filled label="Status"></q-input>
-            <q-input class="q-ma-md" filled label="Senha"></q-input>
-            <q-input class="q-ma-md" filled label="Repetir senha"></q-input>
-          </div>
-        </div>
-        <div class="row q-ma-md">
-          <q-btn class="q-mr-md" color="primary" @click="addNovoUsuario">Adicionar</q-btn>
-          <q-btn color="negative" @click="addNovoUsuario">Cancelar</q-btn>
-        </div>
-      </q-card-section>
-    </q-card>
+      </div>
+      <div class="row q-ma-md">
+        <q-btn class="q-mr-md" color="primary" @click="addNovoUsuario">Adicionar</q-btn>
+        <q-btn color="negative" @click="addNovoUsuario">Cancelar</q-btn>
+      </div>
+    </q-card-section>
+  </q-card>
 </template>
 
 <style scoped>
